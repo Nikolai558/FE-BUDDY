72cc310c6acdf31110cb5a4d4bbb41e0bdb1ead0
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-11 09:18:45 -0700

    Added Runways Model

---
+++ FeBuddyLibrary/Models/Runways.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace FeBuddyLibrary.Models
+{
+    // TODO - Can "public class Runways" be Internal/not public?
+    public class Runways
+    {
+        [XmlElement]
+        public List<Runway> Runway { get; set; }
+    }
+}

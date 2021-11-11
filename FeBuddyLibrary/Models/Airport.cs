1c9073506fac96b80694da6b0d63a67222aeb7ea
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-11 08:58:45 -0700

    Added Airport Model

---
+++ FeBuddyLibrary/Models/Airport.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Serialization;
+
+namespace FeBuddyLibrary.Models
+{
+    // TODO - Can "public class Airport" be Internal/not public?
+    public class Airport
+    {
+        [XmlAttribute]
+        public string ID { get; set; }
+
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        [XmlAttribute]
+        public string Elevation { get; set; }
+
+        [XmlAttribute]
+        public string MagVar { get; set; }
+
+        [XmlAttribute]
+        public string Frequency { get; set; }
+
+        [XmlElement]
+        public Location Location { get; set; }
+
+        [XmlElement]
+        public Runways Runways { get; set; }
+    }
+}

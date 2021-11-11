1975661bf21c26978083c6e755512b183d317e3a
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-11 09:18:23 -0700

    Added Location Models
    This file contains three classes: Location, StartLoc, and EndLoc

---
+++ FeBuddyLibrary/Models/Location.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml.Serialization;
+
+
+/// <summary>
+/// These three classes are in one file because they are all the same. However, we need three of them because the way XML Serializes them.
+/// If just one class was used, then the output files that require XML Serialization would be incorrect and cause a bunch of errors.
+/// </summary>
+
+namespace FeBuddyLibrary.Models
+{
+    // TODO - Can "public class Location" be Internal/not public?
+    public class Location
+    {
+        [XmlAttribute]
+        public string Lon { get; set; }
+
+        [XmlAttribute]
+        public string Lat { get; set; }
+    }
+
+    public class StartLoc
+    {
+        [XmlAttribute]
+        public string Lon { get; set; }
+
+        [XmlAttribute]
+        public string Lat { get; set; }
+    }
+
+    public class EndLoc
+    {
+        [XmlAttribute]
+        public string Lon { get; set; }
+
+        [XmlAttribute]
+        public string Lat { get; set; }
+    }
+}

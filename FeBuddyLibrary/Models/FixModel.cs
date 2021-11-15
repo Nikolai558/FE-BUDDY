41bef2f3e99148a0ec424b35399467bbf25b874b
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-15 14:28:39 -0700

    Added Fix Model

---
+++ FeBuddyLibrary/Models/FixModel.cs
@@ -0,0 +1,23 @@
+﻿namespace FeBuddyLibrary.Models
+{
+    public class FixModel
+    {
+        public string Id { get; set; }
+
+        public string Lat { get; set; }
+
+        public string Lon { get; set; }
+
+        public string Catagory { get; set; }
+
+        public string Use { get; set; }
+
+        public string HiArtcc { get; set; }
+
+        public string LoArtcc { get; set; }
+
+        public string Lat_Dec { get; set; }
+
+        public string Lon_Dec { get; set; }
+    }
+}

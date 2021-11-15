5e8f3c2301dc41b5e14e829820c370a87a701fdc
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-15 14:28:07 -0700

    Added Ats Airway Model

---
+++ FeBuddyLibrary/Models/AtsAirwayModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace FeBuddyLibrary.Models
+{
+    public class AtsAirwayModel
+    {
+        public string Id { get; set; }
+
+        public List<atsAwyPointModel> atsAwyPoints { get; set; }
+    }
+}

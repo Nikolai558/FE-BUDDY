a599c0f11c5a0896584415652db7ba3ad10db9c4
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-15 12:23:20 -0700

    Added ARB Model

---
+++ FeBuddyLibrary/Models/ArbModel.cs
@@ -0,0 +1,23 @@
+﻿namespace FeBuddyLibrary.Models
+{
+    public class ArbModel
+    {
+        public string Identifier { get; set; }
+
+        public string CenterName { get; set; }
+
+        public string DecodeName { get; set; }
+
+        public string Lat { get; set; }
+
+        public string Lon { get; set; }
+
+        public string Description { get; set; }
+
+        public string Sequence { get; set; }
+
+        public string Legal { get; set; }
+
+        public bool ToBeginingAfterThis { get; set; }
+    }
+}

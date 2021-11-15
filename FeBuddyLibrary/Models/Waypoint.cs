eb378bf87dbba95031db83a83616a02c069d6feb
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-15 14:36:22 -0700

    Added Waypoint

---
+++ FeBuddyLibrary/Models/Waypoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace FeBuddyLibrary.Models
+{
+    class Waypoint
+    {
+        [XmlAttribute]
+        public string Type { get; set; }
+
+        [XmlAttribute]
+        public string ID { get; set; }
+
+        [XmlElement]
+        public Location Location { get; set; }
+    }
+}

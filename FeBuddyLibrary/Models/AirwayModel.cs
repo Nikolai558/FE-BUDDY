bc764090eb94a256258e1b6af176a810e4eb1852
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-15 12:22:57 -0700

    Added Airway Model

---
+++ FeBuddyLibrary/Models/AirwayModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace FeBuddyLibrary.Models
+{
+    public class AirwayModel
+    {
+        public string Id { get; set; }
+
+        public List<AwyPointModel> AwyPoints { get; set; }
+    }
+}

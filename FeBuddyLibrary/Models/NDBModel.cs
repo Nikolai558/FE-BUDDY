9b0af06f0d41a47e38f62f98393051d1e3c3d305
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-15 14:28:48 -0700

    Added NDB Model

---
+++ FeBuddyLibrary/Models/NDBModel.cs
@@ -0,0 +1,21 @@
+﻿namespace FeBuddyLibrary.Models
+{
+    public class NDBModel
+    {
+        public string Id { get; set; }
+
+        public string Freq { get; set; }
+
+        public string Lat { get; set; }
+
+        public string Lon { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public string Dec_Lat { get; set; }
+
+        public string Dec_Lon { get; set; }
+    }
+}

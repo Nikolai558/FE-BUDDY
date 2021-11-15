691c81bc553a4ba49a57ea090af98409a5df6eab
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-15 14:28:23 -0700

    Added Ats Awy Point Model

---
+++ FeBuddyLibrary/Models/atsAwyPointModel.cs
@@ -0,0 +1,27 @@
+﻿namespace FeBuddyLibrary.Models
+{
+    public class atsAwyPointModel
+    {
+        public string AirwayId { get; set; }
+
+        public string PointId { get; set; }
+
+        public string Type { get; set; }
+
+        public string Lat { get; set; }
+
+        public string Lon { get; set; }
+
+        public string Name { get; set; }
+
+        public bool GapAfter { get; set; }
+
+        public bool BorderAfter { get; set; }
+
+        public bool EndOfAirway { get; set; }
+
+        public string Dec_Lat { get; set; }
+
+        public string Dec_Lon { get; set; }
+    }
+}

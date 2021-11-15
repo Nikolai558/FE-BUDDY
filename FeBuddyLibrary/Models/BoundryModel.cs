0932f96e5cab27abbdfaa8ce294eee5a0f898adb
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-15 14:28:31 -0700

    Added Boundary Model

---
+++ FeBuddyLibrary/Models/BoundryModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace FeBuddyLibrary.Models
+{
+    public class BoundryModel
+    {
+        public string Identifier { get; set; }
+
+        public string Type { get; set; }
+
+        public List<ArbModel> AllPoints { get; set; }
+    }
+}

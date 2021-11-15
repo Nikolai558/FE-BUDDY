aedd9a3e604782fa06a3b9e052a75c52667270f6
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-15 12:23:07 -0700

    added Apt Model

---
+++ FeBuddyLibrary/Models/AptModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace FeBuddyLibrary.Models
+{
+    public class AptModel
+    {
+        public string Type { get; set; }
+
+        // FAA ID 3-LD
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Lat { get; set; }
+
+        public string Lon { get; set; }
+
+        public string Elv { get; set; }
+
+        public string ResArtcc { get; set; }
+
+        // Facility Status. ie. "O"  - OPERATIONAL, "CP" - CLOSED PERMANENTLY, "CI" - CLOSED INDEFINITELY
+        public string Status { get; set; }
+
+        public string Twr { get; set; }
+
+        public string Ctaf { get; set; }
+
+        public string Icao { get; set; }
+
+        public string Lat_Dec { get; set; }
+
+        public string Lon_Dec { get; set; }
+
+        public string magVariation { get; set; }
+
+        public List<RunwayModel> Runways { get; set; }
+    }
+}

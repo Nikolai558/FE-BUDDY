cd2ff9e1b1b45917c32005698213db18272b5e98
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-15 14:36:03 -0700

    Added Telephoney Model

---
+++ FeBuddyLibrary/Models/TelephonyModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FeBuddyLibrary.Models
+{
+    public class TelephonyModel
+    {
+        public string ThreeLD { get; set; }
+
+        public string Telephony { get; set; }
+
+        public string TelephonyAltered { get; set; }
+    }
+}

89b783ed960f831d91a23b13b1a550fce14e9535
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-15 18:44:37 -0700

    Ready for releasing 0.9.0

--- FeBuddyWinFormUI/Properties/AssemblyInfo.cs
+++ FeBuddyWinFormUI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FeBuddyWinFormUI")]
+[assembly: AssemblyTitle("FE-BUDDY")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FeBuddyWinFormUI")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("FE-BUDDY")]
+[assembly: AssemblyCopyright("Copyright ©  2021 Nikolas Boling, Kyle Sanders")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

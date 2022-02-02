c41a402fbac8bab0207091130a7350e31decc5ab
Caelan Sayler <git@caesay.com>
2022-02-02 22:22:18 +0000

    Apparently WinForms does not like this configured by manifest

--- FeBuddyWinFormUI/Program.cs
+++ FeBuddyWinFormUI/Program.cs
@@ -1,4 +1,4 @@
-﻿using FeBuddyLibrary;
+using FeBuddyLibrary;
 using FeBuddyLibrary.Helpers;
 using Squirrel;
 using System;
@@ -27,14 +27,14 @@ namespace FeBuddyWinFormUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            // CS: we should set DPI awareness as PerMonitorV2 in the application manifest
-            // ideally, however currently this currently causes the application to break on
+            // CS: we should set DPI awareness as PerMonitorV2
+            // however currently this currently causes the application to break on
             // high-dpi monitors since the forms have not been re-written to accomodate the 
             // new scaling requirements. See the following for more details:
             // - https://docs.microsoft.com/en-us/windows/win32/hidpi/setting-the-default-dpi-awareness-for-a-process
             // - https://docs.microsoft.com/en-us/dotnet/desktop/winforms/high-dpi-support-in-windows-forms?view=netframeworkdesktop-4.8
 
-            //Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.SetHighDpiMode(HighDpiMode.DpiUnaware);
 
             DirectoryHelpers.CheckTempDir();
 

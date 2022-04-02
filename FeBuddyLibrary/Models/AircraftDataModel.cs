1a1b8ede1ead073d3f253e98d20d8042235c202f
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-02 13:22:34 -0600

    Added Model For Aircraft Data

---
+++ FeBuddyLibrary/Models/AircraftDataModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeBuddyLibrary.Models
+{
+    public class AircraftDataRootObject
+    {
+        public AircraftDataInformation[] AllAircraftData { get; set; }
+    }
+
+    public class AircraftDataInformation
+    {
+        public string ModelFullName { get; set; }
+        public string Description { get; set; }
+        public string WTC { get; set; }
+        public string WTG { get; set; }
+        public string Designator { get; set; }
+        public string ManufacturerCode { get; set; }
+        public string AircraftDescription { get; set; }
+        public string EngineCount { get; set; }
+        public string EngineType { get; set; }
+    }
+
+}

3d789cf604f214b84445d0250b5c5a2750122de8
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-02 13:23:26 -0600

    Added class incharge of Aircraft Data

---
+++ FeBuddyLibrary/DataAccess/AircraftData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeBuddyLibrary.DataAccess
+{
+    public class AircraftData
+    {
+        public void CreateAircraftDataAlias()
+        {
+            
+        }
+
+        private void GetACData()
+        {
+
+        }
+
+        private void WriteACData()
+        {
+
+        }
+    }
+}

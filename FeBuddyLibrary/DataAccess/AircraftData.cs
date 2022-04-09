58421e22f811d838eb185b1257ce858f87689e13
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-09 13:49:34 -0600

    Converted Json to C# Model

--- FeBuddyLibrary/DataAccess/AircraftData.cs
+++ FeBuddyLibrary/DataAccess/AircraftData.cs
@@ -1,24 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using FeBuddyLibrary.Models;
+using Newtonsoft.Json;
 
 namespace FeBuddyLibrary.DataAccess
 {
     public class AircraftData
     {
-        public void CreateAircraftDataAlias()
+        private AircraftDataRootObject AllACData = new();
+
+        public async void CreateAircraftDataAlias()
         {
-            
+            AircraftDataRootObject aircraftData = await GetACDataAsync();
+            WriteACData(aircraftData);
         }
 
-        private void GetACData()
+        private async Task<AircraftDataRootObject> GetACDataAsync()
         {
+            HttpClient client = new HttpClient();
 
+            var url = "https://www4.icao.int/doc8643/External/AircraftTypes";
+            var values = new Dictionary<string, string>
+            {
+                {"Connection", "keep-alive" },
+                {"Content-Length", "0" },
+                {"Pragma", "no-cache" },
+                {"Cache-Control", "no-cache" },
+                {"Content-Type", "application/json" }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+            var response = await client.PostAsync(url, content);
+
+            string responseStringJson = await response.Content.ReadAsStringAsync();
+
+            responseStringJson = "{\"AllAircraftData\":" + responseStringJson + "}";
+
+            AircraftDataRootObject output = JsonConvert.DeserializeObject<AircraftDataRootObject>(responseStringJson);
+
+            return output;
         }
 
-        private void WriteACData()
+        private void WriteACData(AircraftDataRootObject aircraftData)
         {
 
         }

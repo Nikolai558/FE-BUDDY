14464f3aaa5d8bcce1737aa5b169cba8a0d061e7
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-12 17:02:51 -0600

    Started Dxf to SCT conversion
    Added class for this, no work as actually been done.

---
+++ FeBuddyLibrary/Dxf/Data/DxfSct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeBuddyLibrary.Dxf.Data
+{
+    public class DxfSct
+    {
+        public DxfSct()
+        {
+
+        }
+    }
+}

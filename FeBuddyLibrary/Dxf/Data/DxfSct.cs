c3c5b78d6f2bbcf76bbd49e7c4290a2390e1cb42
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-04-12 22:23:20 -0600

    Converts GeoDxf to SctModel
    need to handle colors from dxf somehow
    need to put converted sctmodel in text file

--- FeBuddyLibrary/Dxf/Data/DxfSct.cs
+++ FeBuddyLibrary/Dxf/Data/DxfSct.cs
@@ -1,16 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FeBuddyLibrary.Dxf.Models;
+using FeBuddyLibrary.Helpers;
+using netDxf;
+using netDxf.Entities;
 
 namespace FeBuddyLibrary.Dxf.Data
 {
     public class DxfSct
     {
+        List<string> _dxfFileSB;
+
         public DxfSct()
         {
+            ////var test = DxfDocument.Load(@"C:\Users\nikol\Desktop\DXF Conversions\All.dxf");
+            //DxfDocument doc = new DxfDocument();
+            //Line entity = new Line();
+            //doc.AddEntity(entity);
+            //doc.Save(@"C:\Users\nikol\Desktop\DXF Conversions\testfromlib.dxf");
+            _dxfFileSB = readDxfFile(@"C:\Users\nikol\Desktop\DXF Conversions\All.dxf");
+            List<SctGeoModel> geoModels = geoParser();
+        }
 
+        private List<SctGeoModel> geoParser()
+        {
+            bool isInLineSection = false;
+            bool isInGeoSection = false;
+
+            List<SctGeoModel> models = new List<SctGeoModel>();
+            SctGeoModel currentModel = null;
+
+
+            int currentIndex = -1;
+            string previous_line = "";
+            foreach (string current_line in _dxfFileSB)
+            {
+                string _currentLine = current_line.Trim();
+                currentIndex += 1;
+                if (previous_line == "0" && _currentLine == "LINE")
+                {
+                    isInLineSection = true;
+                }
+                if (isInLineSection && _currentLine.Contains("GEO__"))
+                {
+                    if (currentModel is not null)
+                    {
+                        models.Add(currentModel);
+                    }
+                    currentModel = new SctGeoModel();
+                    isInGeoSection = true;
+                }
+
+                if (isInLineSection && isInGeoSection)
+                {
+                    switch (_currentLine)
+                    {
+                        case "10": { currentModel.StartLon = LatLonHelpers.CreateDMS(double.Parse(_dxfFileSB[currentIndex + 1].Trim()), false); break; }
+                        case "20": { currentModel.StartLat = LatLonHelpers.CreateDMS(double.Parse(_dxfFileSB[currentIndex + 1].Trim()), true); break; }
+                        case "11": { currentModel.EndLon = LatLonHelpers.CreateDMS(double.Parse(_dxfFileSB[currentIndex + 1].Trim()),false); break; }
+                        case "21": { currentModel.EndLat = LatLonHelpers.CreateDMS(double.Parse(_dxfFileSB[currentIndex + 1].Trim()), true); isInGeoSection = false; isInLineSection = false; break; }
+                        default: { break; }
+                    }
+                }
+                previous_line = _currentLine;
+            }
+            if (currentModel is not null)
+            {
+                models.Add(currentModel);
+            }
+
+            return models;
+        }
+
+
+
+        public List<string> readDxfFile(string filePath)
+        {
+
+            // Might be able to trim down the string builder. but for now just turn the entire dxf file into a string builder...
+            List<string> output = new List<string>();
+
+            foreach (string line in File.ReadLines(filePath))
+            {
+                output.Add(line);
+            }
+            return output;
         }
     }
 }
--- FeBuddyLibrary/Dxf/Data/SctDxf.cs
+++ FeBuddyLibrary/Dxf/Data/SctDxf.cs
@@ -6,26 +6,32 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using netDxf;
 
 namespace FeBuddyLibrary.Dxf.Data
 {
     public class SctDxf
     {
         private readonly SctFileModel _sctFileModel;
+        private StringBuilder OneFileSB = new StringBuilder();
 
         public SctDxf(SctFileModel SctFileModel)
         {
+            string _outputFilePath = @"C:\Users\nikol\Desktop\DXF Conversions";
+
             _sctFileModel = SctFileModel;
-            CreateSidDxf();
-            CreateStarDxf();
-            CreateAirportsDxf();
-            CreateSctArtccModelStuff();
-            CreateNdbAndVorDxf();
-            CreateFixesDxf();
-            CreateRunwayDxf();
-            CreateGeoDxf();
-            CreateRegionDxf();
-            CreaeteLabelsDxf();
+            CreateNdbAndVorDxf(); // [NDB] [VOR]
+            CreateAirportsDxf(); // [AIRPORT]
+            CreateRunwayDxf(); // [RUNWAY]
+            CreateFixesDxf(); // [FIXES]
+            CreateSctArtccModelStuff(); // [ARTCC] [ARTCC_HIGH] [ARTCC_LOW] [LOW_AIRWAY] [HIGH_AIRWAY]
+            CreateSidDxf(); // [SID]
+            CreateStarDxf(); // [STAR]
+            CreateGeoDxf(); // [GEO]
+            CreateRegionDxf(); // [REGIONS]
+            CreaeteLabelsDxf(); // [LABELS]
+
+            //File.WriteAllText(_outputFilePath + "\\All.dxf", OneFileSB.ToString());
         }
 
         private void CreaeteLabelsDxf()
@@ -38,12 +44,12 @@ namespace FeBuddyLibrary.Dxf.Data
 
             foreach (var model in _sctFileModel.SctLabelSection)
             {
-                sb.AppendLine($"  0\nINSERT\n  8\nLABELS\n  2\nLABELS");
+                sb.AppendLine($"  0\nINSERT\n  8\nLABELS__\n  2\nLABELS");
                 sb.AppendLine(" 10");
                 sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(model.Lon, false, false), false));
                 sb.AppendLine(" 20");
                 sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(model.Lat, true, false), false));
-                sb.AppendLine($"  0\nTEXT\n  8\nLABELS");
+                sb.AppendLine($"  0\nTEXT\n  8\nLABELS__");
                 sb.AppendLine(" 10");
                 sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(model.Lon, false, false), false));
                 sb.AppendLine(" 20");
@@ -53,7 +59,8 @@ namespace FeBuddyLibrary.Dxf.Data
             }
             sb.AppendLine("  0\nENDSEC");
             sb.AppendLine("  0\nEOF");
-            File.WriteAllText(_outputFilePath + $"\\LABELS.dxf", sb.ToString());
+            //File.WriteAllText(_outputFilePath + $"\\LABELS.dxf", sb.ToString());
+            OneFileSB.Append(sb.ToString());
         }
 
         private void CreateRegionDxf()
@@ -71,7 +78,7 @@ namespace FeBuddyLibrary.Dxf.Data
                 // TODO - FIGURE OUT IF CONVERTING E CORDINATES IS NEEDED! 
                 sb.AppendLine("  0\nSECTION\n  2\nENTITIES");
                 sb.AppendLine("  0\nLWPOLYLINE\n 5\n100\n  8");
-                sb.AppendLine("REGION_" + diagramName);
+                sb.AppendLine("REGION__" + diagramName);
                 sb.AppendLine("90\n    " + verticieCount.ToString());
                 sb.AppendLine("70\n    1\n43\n0.0");
                 sb.AppendLine(" 10");
@@ -94,9 +101,10 @@ namespace FeBuddyLibrary.Dxf.Data
 
                 sb.AppendLine("  0\nENDSEC");
             }
-            sb.AppendLine("  0\nEOF");
+            //sb.AppendLine("  0\nEOF");
 
-            File.WriteAllText(_outputFilePath + @"\REGIONS.dxf", sb.ToString());
+            //File.WriteAllText(_outputFilePath + @"\REGIONS.dxf", sb.ToString());
+            OneFileSB.Append(sb.ToString());
         }
 
         private void CreateGeoDxf()
@@ -111,7 +119,7 @@ namespace FeBuddyLibrary.Dxf.Data
             foreach (var model in _sctFileModel.SctGeoSection)
             {
                 sb.AppendLine("  0\nLINE\n 62\n56\n  8");
-                sb.AppendLine($"GEO_");
+                sb.AppendLine($"GEO__");
                 sb.AppendLine(" 10");
                 sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(model.StartLon, false, false), false));
                 sb.AppendLine(" 20");
@@ -125,6 +133,8 @@ namespace FeBuddyLibrary.Dxf.Data
             sb.AppendLine("  0\nENDSEC");
             sb.AppendLine("  0\nEOF");
             File.WriteAllText(_outputFilePath + $"\\GEO.dxf", sb.ToString());
+            OneFileSB.Append(sb.ToString());
+
         }
 
         private void CreateRunwayDxf()
@@ -139,7 +149,7 @@ namespace FeBuddyLibrary.Dxf.Data
             foreach (var model in _sctFileModel.SctRunwaySection)
             {
                 sb.AppendLine("  0\nLINE\n 8");
-                sb.AppendLine($"RUNWAY {model.RunwayNumber} {model.OppositeRunwayNumber} {model.MagRunwayHeading} {model.OppositeMagRunwayHeading}");
+                sb.AppendLine($"RUNWAY__{model.RunwayNumber} {model.OppositeRunwayNumber} {model.MagRunwayHeading} {model.OppositeMagRunwayHeading}");
                 sb.AppendLine(" 10");
                 sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(model.StartLon, false, false), false));
                 sb.AppendLine(" 20");
@@ -151,8 +161,10 @@ namespace FeBuddyLibrary.Dxf.Data
             }
 
             sb.AppendLine("  0\nENDSEC");
-            sb.AppendLine("  0\nEOF");
-            File.WriteAllText(_outputFilePath + $"\\RUNWAYS.dxf", sb.ToString());
+            //sb.AppendLine("  0\nEOF");
+            //File.WriteAllText(_outputFilePath + $"\\RUNWAYS.dxf", sb.ToString());
+            OneFileSB.Append(sb.ToString());
+
         }
 
         private void CreateFixesDxf()
@@ -167,12 +179,12 @@ namespace FeBuddyLibrary.Dxf.Data
 
             foreach (var model in _sctFileModel.SctFixesSection)
             {
-                sb.AppendLine("  0\nINSERT\n  8\nFIX\n  2\nFIX");
+                sb.AppendLine("  0\nINSERT\n  8\nFIX__\n  2\nFIX");
                 sb.AppendLine(" 10");
                 sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(model.Lon, false, false), false));
                 sb.AppendLine(" 20");
                 sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(model.Lat, true, false), false));
-                sb.AppendLine("  0\nTEXT\n  8\nFIX");
+                sb.AppendLine("  0\nTEXT\n  8\nFIX__");
                 sb.AppendLine(" 10");
                 sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(model.Lon, false, false), false));
                 sb.AppendLine(" 20");
@@ -181,8 +193,10 @@ namespace FeBuddyLibrary.Dxf.Data
                 sb.AppendLine(model.FixName);
             }
             sb.AppendLine("  0\nENDSEC");
-            sb.AppendLine("  0\nEOF");
-            File.WriteAllText(_outputFilePath + @"\FIX.dxf", sb.ToString());
+            //sb.AppendLine("  0\nEOF");
+            //File.WriteAllText(_outputFilePath + @"\FIX.dxf", sb.ToString());
+            OneFileSB.Append(sb.ToString());
+
         }
 
         private void CreateSidDxf()
@@ -199,7 +213,7 @@ namespace FeBuddyLibrary.Dxf.Data
                 // TODO - FIGURE OUT IF CONVERTING E CORDINATES IS NEEDED! 
                 sb.AppendLine("  0\nSECTION\n  2\nENTITIES");
                 sb.AppendLine("  0\nLINE\n 62\n7\n 8");
-                sb.AppendLine("SID_" + diagramName);
+                sb.AppendLine("SID__" + diagramName);
                 sb.AppendLine(" 10");
                 sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(sctSid.StartLon, false, false), false));
                 sb.AppendLine(" 20");
@@ -212,7 +226,7 @@ namespace FeBuddyLibrary.Dxf.Data
                 foreach (SctAditionalDiagramLineSegments lineSegments in sctSid.AdditionalLines)
                 {
                     sb.AppendLine("  0\nLINE\n 62\n7\n  8");
-                    sb.AppendLine("SID_" + diagramName);
+                    sb.AppendLine("SID__" + diagramName);
                     sb.AppendLine(" 10");
                     sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(lineSegments.StartLon, false, false), false));
                     sb.AppendLine(" 20");
@@ -224,9 +238,11 @@ namespace FeBuddyLibrary.Dxf.Data
                 }
                 sb.AppendLine("  0\nENDSEC");
             }
-            sb.AppendLine("  0\nEOF");
+            //sb.AppendLine("  0\nEOF");
 
-            File.WriteAllText(_outputFilePath + @"\SID.dxf", sb.ToString());
+            //File.WriteAllText(_outputFilePath + @"\SID.dxf", sb.ToString());
+            OneFileSB.Append(sb.ToString());
+
         }
 
         private void CreateStarDxf()
@@ -243,7 +259,7 @@ namespace FeBuddyLibrary.Dxf.Data
                 // TODO - FIGURE OUT IF CONVERTING E CORDINATES IS NEEDED! 
                 sb.AppendLine("  0\nSECTION\n  2\nENTITIES");
                 sb.AppendLine("  0\nLINE\n 62\n7\n 8");
-                sb.AppendLine("STAR_" + diagramName);
+                sb.AppendLine("STAR__" + diagramName);
                 sb.AppendLine(" 10");
                 sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(sctSid.StartLon, false, false), false));
                 sb.AppendLine(" 20");
@@ -256,7 +272,7 @@ namespace FeBuddyLibrary.Dxf.Data
                 foreach (SctAditionalDiagramLineSegments lineSegments in sctSid.AdditionalLines)
                 {
                     sb.AppendLine("  0\nLINE\n 62\n7\n  8");
-                    sb.AppendLine("STAR_" + diagramName);
+                    sb.AppendLine("STAR__" + diagramName);
                     sb.AppendLine(" 10");
                     sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(lineSegments.StartLon, false, false), false));
                     sb.AppendLine(" 20");
@@ -268,9 +284,11 @@ namespace FeBuddyLibrary.Dxf.Data
                 }
                 sb.AppendLine("  0\nENDSEC");
             }
-            sb.AppendLine("  0\nEOF");
+            //sb.AppendLine("  0\nEOF");
 
-            File.WriteAllText(_outputFilePath + @"\STAR.dxf", sb.ToString());
+            //File.WriteAllText(_outputFilePath + @"\STAR.dxf", sb.ToString());
+            OneFileSB.Append(sb.ToString());
+
         }
 
         private void CreateAirportsDxf()
@@ -284,12 +302,12 @@ namespace FeBuddyLibrary.Dxf.Data
 
             foreach (SctAirportModel airportModel in _sctFileModel.SctAirportSection)
             {
-                sb.AppendLine("  0\nINSERT\n  8\nAIRPORT\n  2\nAIRPORT");
+                sb.AppendLine("  0\nINSERT\n  8\nAIRPORT__\n  2\nAIRPORT");
                 sb.AppendLine(" 10");
                 sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(airportModel.Lon, false, false), false));
                 sb.AppendLine(" 20");
                 sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(airportModel.Lat, true, false), false));
-                sb.AppendLine("  0\nTEXT\n  8\nAIRPORT");
+                sb.AppendLine("  0\nTEXT\n  8\nAIRPORT__");
                 sb.AppendLine(" 10");
                 sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(airportModel.Lon, false, false), false));
                 sb.AppendLine(" 20");
@@ -298,8 +316,10 @@ namespace FeBuddyLibrary.Dxf.Data
                 sb.AppendLine(airportModel.Id);
             }
             sb.AppendLine("  0\nENDSEC");
-            sb.AppendLine("  0\nEOF");
-            File.WriteAllText(_outputFilePath + @"\AIRPORT.dxf", sb.ToString());
+            //sb.AppendLine("  0\nEOF");
+            //File.WriteAllText(_outputFilePath + @"\AIRPORT.dxf", sb.ToString());
+            OneFileSB.Append(sb.ToString());
+
         }
 
         private void CreateSctArtccModelStuff()
@@ -328,7 +348,7 @@ namespace FeBuddyLibrary.Dxf.Data
                 foreach (SctArtccModel artccModel in sctArtccModelList[diagramType])
                 {
                     sb.AppendLine("  0\nLINE\n 8");
-                    sb.AppendLine(diagramType + "_" + artccModel.Name);
+                    sb.AppendLine(diagramType + "__" + artccModel.Name);
                     sb.AppendLine(" 10");
                     sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(artccModel.StartLon, false, false), false));
                     sb.AppendLine(" 20");
@@ -340,8 +360,10 @@ namespace FeBuddyLibrary.Dxf.Data
                 }
 
                 sb.AppendLine("  0\nENDSEC");
-                sb.AppendLine("  0\nEOF");
-                File.WriteAllText(_outputFilePath + $"\\{diagramType}.dxf", sb.ToString());
+                //sb.AppendLine("  0\nEOF");
+                //File.WriteAllText(_outputFilePath + $"\\{diagramType}.dxf", sb.ToString());
+                OneFileSB.Append(sb.ToString());
+
             }
         }
 
@@ -365,12 +387,12 @@ namespace FeBuddyLibrary.Dxf.Data
 
                 foreach (var model in sctArtccModelList[diagramType])
                 {
-                    sb.AppendLine($"  0\nINSERT\n  8\n{diagramType}\n  2\n{diagramType}");
+                    sb.AppendLine($"  0\nINSERT\n  8\n{diagramType}__\n  2\n{diagramType}");
                     sb.AppendLine(" 10");
                     sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(model.Lon, false, false), false));
                     sb.AppendLine(" 20");
                     sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(model.Lat, true, false), false));
-                    sb.AppendLine($"  0\nTEXT\n  8\n{diagramType}");
+                    sb.AppendLine($"  0\nTEXT\n  8\n{diagramType}__");
                     sb.AppendLine(" 10");
                     sb.AppendLine("  " + LatLonHelpers.CreateDecFormat(LatLonHelpers.CorrectLatLon(model.Lon, false, false), false));
                     sb.AppendLine(" 20");
@@ -379,8 +401,10 @@ namespace FeBuddyLibrary.Dxf.Data
                     sb.AppendLine(model.Id);
                 }
                 sb.AppendLine("  0\nENDSEC");
-                sb.AppendLine("  0\nEOF");
-                File.WriteAllText(_outputFilePath + $"\\{diagramType}.dxf", sb.ToString());
+                //sb.AppendLine("  0\nEOF");
+                //File.WriteAllText(_outputFilePath + $"\\{diagramType}.dxf", sb.ToString());
+                OneFileSB.Append(sb.ToString());
+
             }
         }
     }

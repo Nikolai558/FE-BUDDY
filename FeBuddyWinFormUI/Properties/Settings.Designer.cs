eee8f1e31c82e84397b28761361efaaaf7b31205
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-07-21 08:37:34 -0600

    User Preference for Artcc id saved

--- FeBuddyWinFormUI/Properties/Settings.Designer.cs
+++ FeBuddyWinFormUI/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace FeBuddyWinFormUI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -32,6 +32,18 @@ namespace FeBuddyWinFormUI.Properties {
             }
             set {
                 this["AllowPreRelease"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int UserArtccSetting {
+            get {
+                return ((int)(this["UserArtccSetting"]));
+            }
+            set {
+                this["UserArtccSetting"] = value;
             }
         }
     }

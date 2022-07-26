74e17b02fdf1ad8963fa594cc2d210864dff4d38
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-07-25 21:45:59 -0600

    change Padding on Airway SCT files.

--- FeBuddyLibrary/DataAccess/GetAtsAwyData.cs
+++ FeBuddyLibrary/DataAccess/GetAtsAwyData.cs
@@ -201,7 +201,7 @@ namespace FeBuddyLibrary.DataAccess
                     }
                     else
                     {
-                        sb.AppendLine($"{airway.Id.PadRight(27)}{prevPoint.Lat} {prevPoint.Lon} {point.Lat} {point.Lon} ;{prevPoint.PointId.PadRight(5)} {point.PointId.PadRight(5)}");
+                        sb.AppendLine($"{airway.Id.PadRight(26)}{prevPoint.Lat} {prevPoint.Lon} {point.Lat} {point.Lon} ;{prevPoint.PointId.PadRight(5)} {point.PointId.PadRight(5)}");
                         GlobalConfig.AwyGeoMap.AppendLine($"            <Element xsi:type=\"Line\" Filters=\"\" StartLat=\"{prevPoint.Dec_Lat}\" StartLon=\"{prevPoint.Dec_Lon}\" EndLat=\"{point.Dec_Lat}\" EndLon=\"{point.Dec_Lon}\" />");
 
                         if (point.GapAfter)

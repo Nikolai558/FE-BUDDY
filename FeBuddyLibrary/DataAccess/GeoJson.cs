16259c3f2c4faa04c1a6d1b4bd3ecf43244e9b06
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-08-05 20:36:02 -0600

    Write Test Files for GeoObjects

--- FeBuddyLibrary/DataAccess/GeoJson.cs
+++ FeBuddyLibrary/DataAccess/GeoJson.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using FeBuddyLibrary.Models;
+using Newtonsoft.Json.Linq;
 
 namespace FeBuddyLibrary.DataAccess
 {
     public class GeoJson
     {
-
-        public void ReadGeoMap(string filepath)
+        public GeoMapSet ReadGeoMap(string filepath)
         {
             XmlSerializer serializer = new XmlSerializer(typeof(GeoMapSet));
             
@@ -23,10 +24,40 @@ namespace FeBuddyLibrary.DataAccess
                 geo = (GeoMapSet)serializer.Deserialize(reader);
             }
 
-            string stop = "STOP";
+            return geo;
         }
 
+        public void WriteGeoJson(string dirPath, GeoMapSet geo)
+        {
+            //string jsonString = JsonSerializer.Serialize(geo, new JsonSerializerOptions()
+            //{
+            //    WriteIndented = true,
+            //});
 
+            //File.WriteAllText(dirPath + "test.json", jsonString);
+
+            foreach (GeoMap geoMap in geo.GeoMaps.GeoMap)
+            {
+                string geoMapDir = Path.Combine(dirPath, geoMap.Name);
+
+                foreach (GeoMapObject geoMapObject in geoMap.Objects.GeoMapObject)
+                {
+                    string fileName = Path.Combine(geoMapDir, geoMapObject.Description + ".geojson");
+                    System.IO.FileInfo file = new System.IO.FileInfo(fileName);
+                    file.Directory.Create(); // If the directory already exists, this method does nothing.
+                    System.IO.File.WriteAllText(file.FullName, "TEST");
+                }
+
+
+            }
+
+
+
+
+
+
+
+        }
 
 
 

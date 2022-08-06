761a7c1281e3b27085cbb38e719f5c7e115b33f5
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-08-05 20:13:43 -0600

    Read GeoMaps

---
+++ FeBuddyLibrary/DataAccess/GeoJson.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using FeBuddyLibrary.Models;
+
+namespace FeBuddyLibrary.DataAccess
+{
+    public class GeoJson
+    {
+
+        public void ReadGeoMap(string filepath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(GeoMapSet));
+            
+            GeoMapSet geo;
+
+            using (Stream reader = new FileStream(filepath, FileMode.Open))
+            {
+                geo = (GeoMapSet)serializer.Deserialize(reader);
+            }
+
+            string stop = "STOP";
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

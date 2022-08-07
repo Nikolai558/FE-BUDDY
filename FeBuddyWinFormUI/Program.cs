5909850a95ee0a521446bc5d93913e15c9f1f7fe
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-08-07 11:17:38 -0600

    remove test code.

--- FeBuddyWinFormUI/Program.cs
+++ FeBuddyWinFormUI/Program.cs
@@ -13,13 +13,6 @@ namespace FeBuddyWinFormUI
         [STAThread]
         static void Main()
         {
-            GeoJson testGeo = new GeoJson();
-            //var geo = testGeo.ReadGeoMap(@"C:\Users\nikol\Downloads\ZDV_GeoMaps.xml");
-            //testGeo.WriteGeoMapGeoJson(@"C:\Users\nikol\Downloads\test\", geo);
-            var geo = testGeo.ReadVideoMap(@"C:\Users\nikol\Downloads\D01_Video_Maps.xml");
-            testGeo.WriteVideoMapGeoJson(@"C:\Users\nikol\Downloads\test\", geo, "D01_Video_Maps", VideoMapFileFormat.both);
-
-            string stop = "STOP";
             // TODO - Get system info and log it into file first thing. -https://docs.microsoft.com/en-us/previous-versions/windows/embedded/ee436483(v=msdn.10)
             Logger.CreateLogFile();
             SquirrelLogger.Register(); // wire up Squirrel logging to our log file too

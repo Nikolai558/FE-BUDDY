437fc16be821f151905bb9904a8508b0423791ab
Nikolas Boling <38259407+Nikolai558@users.noreply.github.com>
2023-01-13 19:23:34 -0700

    Added Close Btn

--- FE-Buddy-DEV/FeBuddyWPF/View/WelcomeView.xaml.cs
+++ FE-Buddy-DEV/FeBuddyWPF/View/WelcomeView.xaml.cs
@@ -36,5 +36,10 @@ namespace FeBuddyWPF.View
         {
             WindowState = WindowState.Minimized;
         }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }

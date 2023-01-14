3be2802013d7ae50c46bd87098cc9db4e19dd3ce
Nikolas Boling <38259407+Nikolai558@users.noreply.github.com>
2023-01-13 17:47:55 -0700

    MVV Implementation
    Removed Main Window Default View
    Added WelcomeView
    Changed Startup screen to WelcomeView
    Added the following folders, Model, View, ViewModel

--- FE-Buddy-DEV/FeBuddyWPF/View/WelcomeView.xaml.cs
+++ FE-Buddy-DEV/FeBuddyWPF/View/WelcomeView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FeBuddyWPF
+namespace FeBuddyWPF.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for WelcomeView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WelcomeView : Window
     {
-        public MainWindow()
+        public WelcomeView()
         {
             InitializeComponent();
         }

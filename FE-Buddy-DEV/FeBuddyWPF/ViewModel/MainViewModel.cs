b5d55b68f3923962855aef40494fae00f076f1c4
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2023-01-15 22:56:29 -0700

    Added New View

---
+++ FE-Buddy-DEV/FeBuddyWPF/ViewModel/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FeBuddyWPF.ViewModel
+{
+    public class MainViewModel: ViewModelBase
+    {
+        private ViewModelBase _currentChildView;
+
+        public MainViewModel()
+        {
+            ShowFaaDatToGeojsonViewCommand = new ViewModelCommand(ExecuteShowFaaDatToGeojsonViewCommand);
+
+
+            //ExecuteShowFaaDatToGeojsonViewCommand(null);
+        }
+
+        private void ExecuteShowFaaDatToGeojsonViewCommand(object obj)
+        {
+            CurrentChildView = new FaaDatToGeojsonViewModel();
+        }
+
+        public ViewModelBase CurrentChildView
+        {
+            get { return _currentChildView; }
+            set { _currentChildView = value; OnPropertyChanged(nameof(CurrentChildView)); }
+        }
+
+        public ICommand ShowFaaDatToGeojsonViewCommand { get; }
+
+    }
+}
---
+++ FE-Buddy-DEV/FeBuddyWPF/ViewModel/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeBuddyWPF.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

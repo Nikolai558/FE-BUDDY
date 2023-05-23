1d4c3e43180264492b9887e66800e07b97768ffc
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2023-05-22 19:40:40 -0600

    Added Comments

--- FeBuddy/WPF/Windows/StartingWindow/ViewModel/ProgStartingViewModel.cs
+++ FeBuddy/WPF/Windows/StartingWindow/ViewModel/ProgStartingViewModel.cs
@@ -9,5 +9,12 @@ namespace WPF.Windows.StartingWindow.ViewModel
 {
     public class ProgStartingViewModel: ViewModelBase
     {
+
+        public ProgStartingViewModel()
+        {
+            // Update check
+            // Display Update Question
+            // Open Program Main Menu Options
+        }
     }
 }

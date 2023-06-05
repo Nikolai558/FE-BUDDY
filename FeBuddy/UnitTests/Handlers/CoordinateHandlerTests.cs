32e04ece9c9b6b3214049d38c0ea295066e53d76
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2023-06-05 15:45:20 -0600

    Added Comments for Unit Tests

--- FeBuddy/UnitTests/Handlers/CoordinateHandlerTests.cs
+++ FeBuddy/UnitTests/Handlers/CoordinateHandlerTests.cs
@@ -9,6 +9,11 @@ using System.Threading.Tasks;
 namespace UnitTests.Handlers;
 public class CoordinateHandlerTests
 {
+  /// <summary>
+  /// Check to make sure that if you pass in a valid DMS latitude AND longitude, that the method returns true.
+  /// </summary>
+  /// <param name="Lat">string: lattitude</param>
+  /// <param name="Lon">string: longitude</param>
   [Theory]
   [InlineData("N043.31.08.418", "W112.03.50.103")]
   [InlineData("N051.52.16.430", "W176.40.26.800")]
@@ -27,6 +32,11 @@ public class CoordinateHandlerTests
     Assert.True(result);
   }
 
+  /// <summary>
+  /// Check to make sure that if you pass in an invalid DMS latitude OR longitude, that the method returns false.
+  /// </summary>
+  /// <param name="Lat"></param>
+  /// <param name="Lon"></param>
   [Theory]
   [InlineData("043.31.08.418", "W112.03.50.103")]
   [InlineData("N051.52.16.430", "176.40.26.800")]
@@ -47,6 +57,11 @@ public class CoordinateHandlerTests
     Assert.False(result);
   }
 
+  /// <summary>
+  /// Check to make sure that the conversion between DMS and decimal is correct.
+  /// </summary>
+  /// <param name="Dms">string: DMS Latitude OR Longitude</param>
+  /// <param name="ExpectedResult">double: Expected Result of the DMS in decimal format.</param>
   [Theory]
   [InlineData("N045.00.00.000", 45.0)]
   [InlineData("S045.00.00.000", -45.0)]
@@ -67,6 +82,12 @@ public class CoordinateHandlerTests
     Assert.Equal(ExpectedResult, actualResult);
   }
 
+  /// <summary>
+  /// Check to make sure that the conversion between decimal and DMS is correct.
+  /// </summary>
+  /// <param name="Dec">double: Decimal version of the Lattitude OR longitude.</param>
+  /// <param name="isLat">bool: True, if the Decimal passed in is a Lattitude, otherwise False.</param>
+  /// <param name="ExpectedResult">string: DMS Latitude OR Longitude</param>
   [Theory]
   [InlineData(45.0, true, "N045.00.00.000")]
   [InlineData(-45.0, true, "S045.00.00.000")]
@@ -87,6 +108,11 @@ public class CoordinateHandlerTests
     Assert.Equal(ExpectedResult, actualResult);
   }
 
+  /// <summary>
+  /// Verify that a valid decimal latitude AND longitude returns true.
+  /// </summary>
+  /// <param name="Lat">double: Lattitude in Decimal Format</param>
+  /// <param name="Lon">double: Longitude in Decimal Format</param>
   [Theory]
   [InlineData(45.0, 10.0)]
   [InlineData(-45.0, 180)]
@@ -109,6 +135,11 @@ public class CoordinateHandlerTests
     Assert.True(result);
   }
 
+  /// <summary>
+  /// Verify that an invalid decimal latitude OR longitude returns false.
+  /// </summary>
+  /// <param name="Lat">double: Lattitude in Decimal Format</param>
+  /// <param name="Lon">double: Longitude in Decimal Format</param>
   [Theory]
   [InlineData(-112.0639175, 179.111)]
   [InlineData(-91.0, 112.4545484)]
@@ -125,7 +156,12 @@ public class CoordinateHandlerTests
     Assert.False(result);
   }
 
-
+  /// <summary>
+  /// Check to make sure the distance between two DMS coordinates is correct when rounding.
+  /// </summary>
+  /// <param name="PointA">string: First point with Lattitude and Longitude seperated by a space.</param>
+  /// <param name="PointB">string: Second point with Lattitude and Longitude seperated by a space.</param>
+  /// <param name="ExpectedResult">double: Expected result rounded to the nearest whole number.</param>
   [Theory]
   [InlineData("N45.00.00.000 W045.00.00.000", "N50.00.00.000 W050.00.00.000", (double)362)]
   [InlineData("N45.00.00.000 W45.00.00.000", "N50.00.00.000 W50.00.00.000", (double)362)]
@@ -142,6 +178,14 @@ public class CoordinateHandlerTests
     Assert.Equal(ExpectedResult, actualResult);
   }
 
+  /// <summary>
+  /// Check to make sure the distance between two decimal coordinates is correct when rounding.
+  /// </summary>
+  /// <param name="PointALat">double: First point Lattitude</param>
+  /// <param name="PointALon">double: First point Longitude</param>
+  /// <param name="PointBLat">double: Second point Lattitude</param>
+  /// <param name="PointBLon">double: Second point Longitude</param>
+  /// <param name="ExpectedResult">double: Expected distance between the two points when rounded to the nearest whole number.</param>
   [Theory]
   [InlineData(43.5190050, -112.0639175, 41.5472222, -108.3045806, (double)204)]
   [InlineData(45.0, -45.0, 46.0, -46.0, (double)73)]
@@ -160,6 +204,14 @@ public class CoordinateHandlerTests
     Assert.Equal(ExpectedResult, actualResult);
   }
 
+  /// <summary>
+  /// Check to make sure that the distance between two DMS coordinates is correct when not rounding.
+  /// </summary>
+  /// <param name="PointALat">double: First point Lattitude<</param>
+  /// <param name="PointALon">double: First point Longitude<</param>
+  /// <param name="PointBLat">double: Second point Lattitude</param>
+  /// <param name="PointBLon">double: Second point Longitude</param>
+  /// <param name="ExpectedResult">double: Expected result for the distance not rounded at all.</param>
   [Theory]
   [InlineData(45, -45, 45.0001, -45.0001, 0.007353)]
   [InlineData(45, -45, 46.665543614161486, -45, 100.0)]
@@ -176,6 +228,14 @@ public class CoordinateHandlerTests
     Assert.Equal(ExpectedResult, actualResult);
   }
 
+  /// <summary>
+  /// Check two coordintes to see if they cross the antimeridian or not.
+  /// </summary>
+  /// <param name="StartLat">double: Starting Lattitude</param>
+  /// <param name="StartLon">double: Starting Longitude</param>
+  /// <param name="EndLat">double: Ending Lattitude</param>
+  /// <param name="EndLon">double: Ending Longitude</param>
+  /// <param name="ExpectedResult">bool: Expected Result, True, if it does cross the antimeridian, otherwise false.</param>
   [Theory]
   [InlineData(35, 179, 35, -179, true)] 
   [InlineData(38, -162, 25, -119, false)]
@@ -190,6 +250,14 @@ public class CoordinateHandlerTests
     Assert.Equal(ExpectedResult, actualResult);
   }
 
+  /// <summary>
+  /// Check the bearing between two coordinates.
+  /// </summary>
+  /// <param name="PointALat">double: First point Lattitude</param>
+  /// <param name="PointALon">double: First point Longitude</param>
+  /// <param name="PointBLat">double: Second point Lattitude</param>
+  /// <param name="PointBLon">double: Second point Longitude</param>
+  /// <param name="ExpectedResult">double: Expected Bearing from Point A to Point B</param>
   [Theory]
   [InlineData(37.4223878, -122.0841877, 52.3752182, 4.8839765, 29.787552309755483)]
   [InlineData(52.3752182, 4.8839765, 37.4223878, -122.0841877, 319.73956262516884)]

b8d13a756887b0de9e053315764130c2ece1adf6
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2023-06-05 15:14:51 -0600

    Added Logic and Calculation for Locations
    Added Bearing calculation
    Added CrossesAntimeridian calculation
    Added Distance calculation
    Added the Logic that was previously in the location Model.
    Added Tests for all methods.

---
+++ FeBuddy/FEBuddyLibrary/Handlers/CoordinateHandler.cs
@@ -0,0 +1,153 @@
+﻿using FEBuddyLibrary.Models.Location;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FEBuddyLibrary.Handlers;
+/// <summary>
+/// A static class that handles all logic for coordinate math and conversions.
+/// </summary>
+public class CoordinateHandler
+{
+  // ---------- Methods ---------- 
+  /// <summary>
+  /// Check a Decimal Latitude and Longitude to see if they are valid.
+  /// </summary>
+  /// <param name="Lat">double: Latitude</param>
+  /// <param name="Lon">double: Longitude</param>
+  /// <returns>bool: Returns true if the Latitude AND Longitude are valid Decimals, otherwise returns False.</returns>
+  public static bool IsValidDecimal(double Lat, double Lon)
+  {
+    if (Lat > 90 || Lat < -90) return false;
+    if (Lon > 180 || Lon < -180) return false;
+    return true;
+  }
+
+  /// <summary>
+  /// Check a DMS Latitude and Longitude to see if they are valid.
+  /// </summary>
+  /// <param name="Lat">string: Latitude - Format: ['N', 'S']DDD.MM.SS.SSS</param>
+  /// <param name="Lon">string: Longitude - Format: ['E', 'W']DDD.MM.SS.SSS</param>
+  /// <returns>bool: Returns True if the Latitude AND Longitude are valid DMS formats, otherwise returns False.</returns>
+  public static bool IsValidDMS(string Lat, string Lon)
+  {
+    if (Lat == null || Lon == null) return false;
+    if (Lat == "" || Lon == "") return false;
+    if (Lat.Split('.').Count() != 4 || Lon.Split('.').Count() != 4) return false;
+    if (Lat[0] != 'N' && Lat[0] != 'S') return false;
+    if (Lon[0] != 'E' && Lon[0] != 'W') return false;
+
+    return true;
+  }
+
+  /// <summary>
+  /// Converts Decimal format to DMS format.
+  /// </summary>
+  /// <param name="Dec">double: Decimal Format for either Latitude or Longitude</param>
+  /// <param name="IsLat">bool: If the Decimal you passed in is a Lattitude, pass "true" for this parameter, otherwise pass "false"</param>
+  /// <returns>string: DMS format of the value passed in. Format: ['N', 'S', 'E', 'W']DDD.MM.SS.SSS</returns>
+  public static string ToDMS(double Dec, bool IsLat)
+  {
+    // get the hemisphere (N, S, E, or W)
+    string hemisphere = "";
+    if (IsLat && Dec < 0) hemisphere = "S";
+    if (IsLat && Dec >= 0) hemisphere = "N";
+    if (!IsLat && Dec < 0) hemisphere = "W";
+    if (!IsLat && Dec >= 0) hemisphere = "E";
+    // get the degrees, minutes, and seconds
+    double absLat = Math.Abs(Dec);
+    double degrees = Math.Floor(absLat);
+    double minutes = Math.Floor((absLat - degrees) * 60);
+    double seconds = Math.Round((absLat - degrees - minutes / 60) * 3600, 3);
+    double miliseconds = (seconds - Math.Floor(seconds)) * 1000;
+    // put it all together
+    string dms = hemisphere + degrees.ToString("000") + "." + minutes.ToString("00") + "." + seconds.ToString("00") + "." + miliseconds.ToString("000");
+    return dms;
+  }
+
+  /// <summary>
+  /// Converts DMS format to Decimal format.
+  /// </summary>
+  /// <param name="DMS">string: Latitude or Longitude - Format: ['N', 'S', 'E', 'W']DDD.MM.SS.SSS</param>
+  /// <returns>double: Returns the decimal version of the Latitude or Longitude passed in.</returns>
+  public static double ToDecimal(string DMS)
+  {
+    string[] parts = DMS.Split('.');
+    double degrees = double.Parse(parts[0][1..]);
+    double minutes = double.Parse(parts[1]);
+    double seconds = double.Parse(parts[2]);
+    double milliseconds = double.Parse(parts[3]);
+    double result = degrees + (minutes / 60) + (seconds / 3600) + (milliseconds / 3600000);
+    if (DMS[0] == 'S' || DMS[0] == 'W') result *= (double)-1;
+    return Math.Round(result, 7);
+  }
+
+  /// <summary>
+  /// Calculate the distance between two points.
+  /// </summary>
+  /// <param name="PointA">Location: First point that contains the Lattitude and Longitude.</param>
+  /// <param name="PointB">Location: Second point that contains the Lattitude and Longitude</param>
+  /// <param name="Round">bool: Round the result to the nearest whole number</param>
+  /// <returns>double: Returns the distance between two points in Nautical Miles.</returns>
+  public static double Distance(Location PointA, Location PointB, bool Round = true)
+  {
+    // convert the points to radians
+    double lat1 = PointA.DecLat * Math.PI / 180;
+    double lon1 = PointA.DecLon * Math.PI / 180;
+    double lat2 = PointB.DecLat * Math.PI / 180;
+    double lon2 = PointB.DecLon * Math.PI / 180;
+    // get the differences between the two points
+    double dLat = lat2 - lat1;
+    double dLon = lon2 - lon1;
+    // do some math
+    double a = Math.Pow(Math.Sin(dLat / 2), 2) + Math.Cos(lat1) * Math.Cos(lat2) * Math.Pow(Math.Sin(dLon / 2), 2);
+    double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+    double d = 6371e3 * c;
+    // convert the distance to nautical miles
+    double nm = d / 1852;
+    // return the result
+    if (Round) return Math.Round(nm, 0);
+    else return Math.Round(nm, 6);
+  }
+
+  /// <summary>
+  /// Check to see if the line between two points crosses the antimeridian.
+  /// </summary>
+  /// <param name="StartPoint">Location: Starting point location.</param>
+  /// <param name="EndPoint">Location: Ending point location</param>
+  /// <returns>bool: Returns True if the two points that form the line crosses the Antimeridian, False if it does Not cross.</returns>
+  public static bool CrossesAntimeridian(Location StartPoint, Location EndPoint)
+  {
+    if (StartPoint.DecLon < 0 && EndPoint.DecLon > 0) return true;
+    else if (StartPoint.DecLon > 0 && EndPoint.DecLon < 0) return true;
+    else return false;
+  }
+
+  /// <summary>
+  /// Get the bearing between two points.
+  /// </summary>
+  /// <param name="pointA">Location: Starting point</param>
+  /// <param name="pointB">Location: Ending point</param>
+  /// <returns>double: Returns the bearing from the starting point to the ending point.</returns>
+  public static double Bearing(Location pointA, Location pointB)
+  {
+    // convert the points to radians
+    double lat1 = pointA.DecLat * Math.PI / 180;
+    double lon1 = pointA.DecLon * Math.PI / 180;
+    double lat2 = pointB.DecLat * Math.PI / 180;
+    double lon2 = pointB.DecLon * Math.PI / 180;
+    // get the differences between the two points
+    double dLon = lon2 - lon1;
+    // do some math
+    double y = Math.Sin(dLon) * Math.Cos(lat2);
+    double x = Math.Cos(lat1) * Math.Sin(lat2) - Math.Sin(lat1) * Math.Cos(lat2) * Math.Cos(dLon);
+    // get the bearing
+    double bearing = Math.Atan2(y, x);
+    bearing = bearing * 180 / Math.PI;
+    bearing = (bearing + 360) % 360;
+    return bearing;
+  }
+}
---
+++ FeBuddy/UnitTests/Handlers/CoordinateHandlerTests.cs
@@ -0,0 +1,209 @@
+﻿using FEBuddyLibrary.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Handlers;
+public class CoordinateHandlerTests
+{
+  [Theory]
+  [InlineData("N043.31.08.418", "W112.03.50.103")]
+  [InlineData("N051.52.16.430", "W176.40.26.800")]
+  [InlineData("N000.00.00.000", "E000.00.00.000")]
+  [InlineData("S000.00.00.000", "E000.00.00.000")]
+  [InlineData("S000.00.00.000", "W112.03.50.103")]
+  [InlineData("N043.31.08.418", "W000.00.00.000")]
+  public void validate_dms_input_should_be_true(string Lat, string Lon)
+  {
+    // Arrange
+
+    // Act
+    bool result = CoordinateHandler.IsValidDMS(Lat, Lon);
+
+    // Assert
+    Assert.True(result);
+  }
+
+  [Theory]
+  [InlineData("043.31.08.418", "W112.03.50.103")]
+  [InlineData("N051.52.16.430", "176.40.26.800")]
+  [InlineData("N043.31.08.418", null)]
+  [InlineData(null, "W112.03.50.103")]
+  [InlineData(null, null)]
+  [InlineData("", "176.40.26.800")]
+  [InlineData("", "")]
+  [InlineData("N043.31.08.418", "")]
+  public void validate_dms_input_should_be_false(string Lat, string Lon)
+  {
+    // Arrange
+
+    // Act
+    bool result = CoordinateHandler.IsValidDMS(Lat, Lon);
+
+    // Assert
+    Assert.False(result);
+  }
+
+  [Theory]
+  [InlineData("N045.00.00.000", 45.0)]
+  [InlineData("S045.00.00.000", -45.0)]
+  [InlineData("E118.00.00.000", 118.0)]
+  [InlineData("W118.00.00.000", -118.0)]
+  [InlineData("N041.32.50.000", 41.5472222)]
+  [InlineData("W112.07.40.000", -112.1277778)]
+  [InlineData("N045.49.43.089", 45.8286358)]
+  [InlineData("W108.18.16.490", -108.3045806)]
+  [InlineData("N043.31.08.418", 43.5190050)]
+  [InlineData("W112.03.50.103", -112.0639175)]
+  public void converting_dms_to_dec_should_be_correct(string Dms, double ExpectedResult)
+  {
+    // Arrange
+    // Act
+    double actualResult = CoordinateHandler.ToDecimal(Dms);
+    // Assert
+    Assert.Equal(ExpectedResult, actualResult);
+  }
+
+  [Theory]
+  [InlineData(45.0, true, "N045.00.00.000")]
+  [InlineData(-45.0, true, "S045.00.00.000")]
+  [InlineData(118.0, false, "E118.00.00.000")]
+  [InlineData(-118.0, false, "W118.00.00.000")]
+  [InlineData(41.5472222, true, "N041.32.50.000")]
+  [InlineData(-112.1277778, false, "W112.07.40.000")]
+  [InlineData(45.8286358, true, "N045.49.43.089")]
+  [InlineData(-108.3045806, false, "W108.18.16.490")]
+  [InlineData(43.5190050, true, "N043.31.08.418")]
+  [InlineData(-112.0639175, false, "W112.03.50.103")]
+  public void converting_dec_to_dms_should_be_correct(double Dec, bool isLat, string ExpectedResult)
+  {
+    // Arrange
+    // Act
+    string actualResult = CoordinateHandler.ToDMS(Dec, isLat);
+    // Assert
+    Assert.Equal(ExpectedResult, actualResult);
+  }
+
+  [Theory]
+  [InlineData(45.0, 10.0)]
+  [InlineData(-45.0, 180)]
+  [InlineData(41, 118.0)]
+  [InlineData(25, -118.0)]
+  [InlineData(41.5472222, 112)]
+  [InlineData(88, -112.1277778)]
+  [InlineData(45.8286358, 179.55)]
+  [InlineData(-88, -108.3045806)]
+  [InlineData(43.5190050, -179.55)]
+  [InlineData(20, -112.0639175)]
+  [InlineData(-90, -180)]
+  [InlineData(90, 180)]
+  public void validate_decimal_input_should_be_true(double Lat, double Lon)
+  {
+    // Arrange
+    // Act
+    bool result = CoordinateHandler.IsValidDecimal(Lat, Lon);
+    // Assert
+    Assert.True(result);
+  }
+
+  [Theory]
+  [InlineData(-112.0639175, 179.111)]
+  [InlineData(-91.0, 112.4545484)]
+  [InlineData(91.0, 118.0)]
+  [InlineData(91.4548156, 118.0)]
+  [InlineData(45.0, -181.1813548)]
+  [InlineData(-45.0, 200.1803658)]
+  public void validate_decimal_input_should_be_false(double Lat, double Lon)
+  {
+    // Arrange
+    // Act
+    bool result = CoordinateHandler.IsValidDecimal(Lat, Lon);
+    // Assert
+    Assert.False(result);
+  }
+
+
+  [Theory]
+  [InlineData("N45.00.00.000 W045.00.00.000", "N50.00.00.000 W050.00.00.000", (double)362)]
+  [InlineData("N45.00.00.000 W45.00.00.000", "N50.00.00.000 W50.00.00.000", (double)362)]
+  [InlineData("N043.31.08.418 W112.03.50.103", "N041.32.50.000 W108.18.16.490", (double)204)]
+  public void distance_Rounded_whole_number_between_two_dms_coords_should_be_correct(string PointA, string PointB, double ExpectedResult)
+  {
+    // Arrange
+    Location _pointA = new Location(PointA.Split(' ')[0], PointA.Split(' ')[1]); ;
+    Location _pointB = new Location(PointB.Split(' ')[0], PointB.Split(' ')[1]); ;
+
+    // Act
+    double actualResult = CoordinateHandler.Distance(_pointA, _pointB);
+    // Assert
+    Assert.Equal(ExpectedResult, actualResult);
+  }
+
+  [Theory]
+  [InlineData(43.5190050, -112.0639175, 41.5472222, -108.3045806, (double)204)]
+  [InlineData(45.0, -45.0, 46.0, -46.0, (double)73)]
+  [InlineData(45.0, -45.0, 45.1, -45.1, (double)7)]
+  [InlineData(45.0, -45.0, 45.0001, -45.0001, (double)0)]
+  public void distance_Rounded_whole_number_between_two_decimal_coords_should_be_correct(double PointALat, double PointALon, double PointBLat, double PointBLon, double ExpectedResult)
+  {
+    // Arrange
+    Location _pointA = new Location(PointALat, PointALon);
+    Location _pointB = new Location(PointBLat, PointBLon);
+
+    // Act
+    double actualResult = CoordinateHandler.Distance(_pointA, _pointB);
+
+    // Assert
+    Assert.Equal(ExpectedResult, actualResult);
+  }
+
+  [Theory]
+  [InlineData(45, -45, 45.0001, -45.0001, 0.007353)]
+  [InlineData(45, -45, 46.665543614161486, -45, 100.0)]
+  public void distance_NOT_ROUNDED_between_two_decimal_coords_should_be_correct(double PointALat, double PointALon, double PointBLat, double PointBLon, double ExpectedResult)
+  {
+    // Arrange
+    Location _pointA = new Location(PointALat, PointALon);
+    Location _pointB = new Location(PointBLat, PointBLon);
+
+    // Act
+    double actualResult = CoordinateHandler.Distance(_pointA, _pointB, Round: false);
+
+    // Assert
+    Assert.Equal(ExpectedResult, actualResult);
+  }
+
+  [Theory]
+  [InlineData(35, 179, 35, -179, true)] 
+  [InlineData(38, -162, 25, -119, false)]
+  public void crosses_the_am_should_be_correct(double StartLat, double StartLon, double EndLat, double EndLon, bool ExpectedResult)
+  {
+    // Arrange
+    Location _pointA = new Location(StartLat, StartLon);
+    Location _pointB = new Location(EndLat, EndLon);
+    // Act
+    bool actualResult = CoordinateHandler.CrossesAntimeridian(_pointA, _pointB);
+    // Assert
+    Assert.Equal(ExpectedResult, actualResult);
+  }
+
+  [Theory]
+  [InlineData(37.4223878, -122.0841877, 52.3752182, 4.8839765, 29.787552309755483)]
+  [InlineData(52.3752182, 4.8839765, 37.4223878, -122.0841877, 319.73956262516884)]
+  [InlineData(45, 45, 46, 45, 0.0)]
+  [InlineData(45, 45, 49, 45.5, 4.6907374307851342)]
+  [InlineData(45, 45, 45.089517481040396, 45.012209062406434, 5.5000000000001137)]
+  public void get_bearing_between_two_decimal_coords_should_be_correct(double PointALat, double PointALon, double PointBLat, double PointBLon, double ExpectedResult)
+  {
+    // Arrange
+    Location _pointA = new Location(PointALat, PointALon); 
+    Location _pointB = new Location(PointBLat, PointBLon);
+    // Act
+    double actualResult = CoordinateHandler.Bearing(_pointA, _pointB);
+    // Assert
+    Assert.Equal(ExpectedResult, actualResult);
+  }
+}

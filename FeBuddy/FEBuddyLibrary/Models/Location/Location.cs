ebee713bb247715e82cd1637ededef087afe9478
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2023-06-05 15:11:14 -0600

    Removed Model Logic
    Moved all location logic and calculations into a seperate class

--- FeBuddy/FEBuddyLibrary/Models/Location/Location.cs
+++ FeBuddy/FEBuddyLibrary/Models/Location/Location.cs
@@ -1,6 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using FEBuddyLibrary.Handlers;
+using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("UnitTests")]
 namespace FEBuddyLibrary.Models.Location;
 
 /// <summary>
@@ -24,14 +24,14 @@ public class Location
   public Location(string Lat, string Lon)
   {
 
-    bool isValid = IsValidDMS(Lat, Lon);
+    bool isValid = CoordinateHandler.IsValidDMS(Lat, Lon);
     if (isValid)
     {
       // Possible Optimization - Do we really need BOTH DMS and DEC at Location Creation Time?
       DmsLat = Lat;
       DmsLon = Lon;
-      DecLat = (double)ToDecimal(Lat);
-      DecLon = (double)ToDecimal(Lon);
+      DecLat = (double)CoordinateHandler.ToDecimal(Lat);
+      DecLon = (double)CoordinateHandler.ToDecimal(Lon);
       return;
     }
     throw new ArgumentException("Invalid DMS input when creating a Location class.");
@@ -45,89 +45,18 @@ public class Location
   /// <exception cref="ArgumentException"></exception>
   public Location(double Lat, double Lon)
   {
-    bool isValid = IsValidDecimal(Lat, Lon);
+    bool isValid = CoordinateHandler.IsValidDecimal(Lat, Lon);
     if (isValid)
     {
       // Possible Optimization - Do we really need BOTH DMS and DEC at Location Creation Time?
       DecLat = Lat;
       DecLon = Lon;
-      DmsLat = ToDMS(Lat, true);
-      DmsLon = ToDMS(Lon, false);
+      DmsLat = CoordinateHandler.ToDMS(Lat, true);
+      DmsLon = CoordinateHandler.ToDMS(Lon, false);
       return;
     }
     throw new ArgumentException("Invalid decimal input when creating a Location class.");
   }
 
-  // ---------- Methods ---------- 
-  /// <summary>
-  /// Check a Decimal Latitude and Longitude to see if they are valid.
-  /// </summary>
-  /// <param name="Lat">double: Latitude</param>
-  /// <param name="Lon">double: Longitude</param>
-  /// <returns>bool: Returns true if the Latitude AND Longitude are valid Decimals, otherwise returns False.</returns>
-  internal static bool IsValidDecimal(double Lat, double Lon)
-  {
-    if (Lat > 90 || Lat < -90) return false;
-    if (Lon > 180 || Lon < -180) return false;
-    return true;
-  }
-
-  /// <summary>
-  /// Check a DMS Latitude and Longitude to see if they are valid.
-  /// </summary>
-  /// <param name="Lat">string: Latitude - Format: ['N', 'S']DDD.MM.SS.SSS</param>
-  /// <param name="Lon">string: Longitude - Format: ['E', 'W']DDD.MM.SS.SSS</param>
-  /// <returns>bool: Returns True if the Latitude AND Longitude are valid DMS formats, otherwise returns False.</returns>
-  internal static bool IsValidDMS(string Lat, string Lon)
-  {
-    if (Lat == null || Lon == null) return false;
-    if (Lat == "" || Lon == "") return false;
-    if (Lat.Split('.').Count() != 4 || Lon.Split('.').Count() != 4) return false;
-    if (Lat[0] != 'N' && Lat[0] != 'S') return false;
-    if (Lon[0] != 'E' && Lon[0] != 'W') return false;
-
-    return true;
-  }
-
-  /// <summary>
-  /// Converts Decimal format to DMS format.
-  /// </summary>
-  /// <param name="Dec">double: Decimal Format for either Latitude or Longitude</param>
-  /// <param name="IsLat">bool: If the Decimal you passed in is a Lattitude, pass "true" for this parameter, otherwise pass "false"</param>
-  /// <returns>string: DMS format of the value passed in. Format: ['N', 'S', 'E', 'W']DDD.MM.SS.SSS</returns>
-  internal static string ToDMS(double Dec, bool IsLat)
-  {
-    // get the hemisphere (N, S, E, or W)
-    string hemisphere = "";
-    if (IsLat && Dec < 0) hemisphere = "S";
-    if (IsLat && Dec >= 0) hemisphere = "N";
-    if (!IsLat && Dec < 0) hemisphere = "W";
-    if (!IsLat && Dec >= 0) hemisphere = "E";
-    // get the degrees, minutes, and seconds
-    double absLat = Math.Abs(Dec);
-    double degrees = Math.Floor(absLat);
-    double minutes = Math.Floor((absLat - degrees) * 60);
-    double seconds = Math.Round((absLat - degrees - minutes / 60) * 3600, 3);
-    double miliseconds = (seconds - Math.Floor(seconds)) * 1000;
-    // put it all together
-    string dms = hemisphere + degrees.ToString("000") + "." + minutes.ToString("00") + "." + seconds.ToString("00") + "." + miliseconds.ToString("000");
-    return dms;
-  }
-
-  /// <summary>
-  /// Converts DMS format to Decimal format.
-  /// </summary>
-  /// <param name="DMS">string: Latitude or Longitude - Format: ['N', 'S', 'E', 'W']DDD.MM.SS.SSS</param>
-  /// <returns>double: Returns the decimal version of the Latitude or Longitude passed in.</returns>
-  internal static double ToDecimal(string DMS)
-  {
-    string[] parts = DMS.Split('.');
-    double degrees = double.Parse(parts[0][1..]);
-    double minutes = double.Parse(parts[1]);
-    double seconds = double.Parse(parts[2]);
-    double milliseconds = double.Parse(parts[3]);
-    double result = degrees + (minutes / 60) + (seconds / 3600) + (milliseconds / 3600000);
-    if (DMS[0] == 'S' || DMS[0] == 'W') result *= (double)-1;
-    return Math.Round(result, 7);
-  }
+  
 }
--- FeBuddy/UnitTests/Library/Models/LocationTests.cs
+++ FeBuddy/UnitTests/Library/Models/LocationTests.cs
@@ -2,46 +2,6 @@
 
 public class LocationTests
 {
-
-  [Theory]
-  [InlineData("N043.31.08.418", "W112.03.50.103")]
-  [InlineData("N051.52.16.430", "W176.40.26.800")]
-  [InlineData("N000.00.00.000", "E000.00.00.000")]
-  [InlineData("S000.00.00.000", "E000.00.00.000")]
-  [InlineData("S000.00.00.000", "W112.03.50.103")]
-  [InlineData("N043.31.08.418", "W000.00.00.000")]
-  public void validate_dms_input_should_be_true(string Lat, string Lon)
-  {
-    // Arrange
-
-    // Act
-    bool result = Location.IsValidDMS(Lat, Lon);
-
-    // Assert
-    Assert.True(result);
-  }
-
-  [Theory]
-  [InlineData("043.31.08.418", "W112.03.50.103")]
-  [InlineData("N051.52.16.430", "176.40.26.800")]
-  [InlineData("N043.31.08.418", null)]
-  [InlineData(null, "W112.03.50.103")]
-  [InlineData(null, null)]
-  [InlineData("", "176.40.26.800")]
-  [InlineData("", "")]
-  [InlineData("N043.31.08.418", "")]
-  public void validate_dms_input_should_be_false(string Lat, string Lon)
-  {
-    // Arrange
-
-    // Act
-    bool result = Location.IsValidDMS(Lat, Lon);
-
-    // Assert
-    Assert.False(result);
-  }
-
-
   [Theory]
   [InlineData("N041.32.50.000", "W108.18.16.490", 41.5472222, -108.3045806)]
   [InlineData("N043.31.08.418", "W112.03.50.103", 43.5190050, -112.0639175)]
@@ -57,7 +17,6 @@ public class LocationTests
     Assert.Equal(DecLat, loc.DecLat);
     Assert.Equal(DecLon, loc.DecLon);
   }
-
 
   [Theory]
   [InlineData(41.5472222, -108.3045806, "N041.32.50.000", "W108.18.16.490")]
@@ -75,81 +34,4 @@ public class LocationTests
     Assert.Equal(DmsLon, loc.DmsLon);
   }
 
-  [Theory]
-  [InlineData("N045.00.00.000", 45.0)]
-  [InlineData("S045.00.00.000", -45.0)]
-  [InlineData("E118.00.00.000", 118.0)]
-  [InlineData("W118.00.00.000", -118.0)]
-  [InlineData("N041.32.50.000", 41.5472222)]
-  [InlineData("W112.07.40.000", -112.1277778)]
-  [InlineData("N045.49.43.089", 45.8286358)]
-  [InlineData("W108.18.16.490", -108.3045806)]
-  [InlineData("N043.31.08.418", 43.5190050)]
-  [InlineData("W112.03.50.103", -112.0639175)]
-  public void converting_dms_to_dec_should_be_correct(string Dms, double ExpectedResult)
-  {
-    // Arrange
-    // Act
-    double actualResult = Location.ToDecimal(Dms);
-    // Assert
-    Assert.Equal(ExpectedResult, actualResult);
-  }
-
-  [Theory]
-  [InlineData(45.0, true, "N045.00.00.000")]
-  [InlineData(-45.0, true, "S045.00.00.000")]
-  [InlineData(118.0, false, "E118.00.00.000")]
-  [InlineData(-118.0, false, "W118.00.00.000")]
-  [InlineData(41.5472222, true, "N041.32.50.000")]
-  [InlineData(-112.1277778, false, "W112.07.40.000")]
-  [InlineData(45.8286358, true, "N045.49.43.089")]
-  [InlineData(-108.3045806, false, "W108.18.16.490")]
-  [InlineData(43.5190050, true, "N043.31.08.418")]
-  [InlineData(-112.0639175, false, "W112.03.50.103")]
-  public void converting_dec_to_dms_should_be_correct(double Dec, bool isLat, string ExpectedResult)
-  {
-    // Arrange
-    // Act
-    string actualResult = Location.ToDMS(Dec, isLat);
-    // Assert
-    Assert.Equal(ExpectedResult, actualResult);
-  }
-
-  [Theory]
-  [InlineData(45.0, 10.0)]
-  [InlineData(-45.0, 180)]
-  [InlineData(41, 118.0)]
-  [InlineData(25, -118.0)]
-  [InlineData(41.5472222, 112)]
-  [InlineData(88, -112.1277778)]
-  [InlineData(45.8286358, 179.55)]
-  [InlineData(-88, -108.3045806)]
-  [InlineData(43.5190050, -179.55)]
-  [InlineData(20, -112.0639175)]
-  [InlineData(-90, -180)]
-  [InlineData(90, 180)]
-  public void validate_decimal_input_should_be_true(double Lat, double Lon)
-  {
-    // Arrange
-    // Act
-    bool result = Location.IsValidDecimal(Lat, Lon);
-    // Assert
-    Assert.True(result);
-  }
-
-  [Theory]
-  [InlineData(-112.0639175, 179.111)]
-  [InlineData(-91.0, 112.4545484)]
-  [InlineData(91.0, 118.0)]
-  [InlineData(91.4548156, 118.0)]
-  [InlineData(45.0, -181.1813548)]
-  [InlineData(-45.0, 200.1803658)]
-  public void validate_decimal_input_should_be_false(double Lat, double Lon)
-  {
-    // Arrange
-    // Act
-    bool result = Location.IsValidDecimal(Lat, Lon);
-    // Assert
-    Assert.False(result);
-  }
 }

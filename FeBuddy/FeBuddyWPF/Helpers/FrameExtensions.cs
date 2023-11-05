e18c9ff2b9de43b5a3cc99bfd1379b939e248ae1
Nikolas Boling <38259407+Nikolai558@users.noreply.github.com>
2023-11-04 18:50:43 -0700

    Added Frame Extension Helper

---
+++ FeBuddy/FeBuddyWPF/Helpers/FrameExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+// This namespace contains the helper classes related to the FeBuddyWPF application.
+namespace FeBuddyWPF.Helpers
+{
+    // This static class provides extension methods for the Frame class.
+    public static class FrameExtensions
+    {
+        // Retrieves the data context from the Frame's content if available.
+        public static object GetDataContext(this Frame frame)
+        {
+            if (frame.Content is FrameworkElement element)
+            {
+                return element.DataContext;
+            }
+
+            return null;
+        }
+
+        // Clears the navigation history of the Frame.
+        public static void CleanNavigation(this Frame frame)
+        {
+            while (frame.CanGoBack)
+            {
+                frame.RemoveBackEntry();
+            }
+        }
+    }
+}

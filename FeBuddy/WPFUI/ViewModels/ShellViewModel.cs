e98c56cb9b4a56252f0dfafa03c16eb2dfcc5eda
Nikolas Boling <38259407+Nikolai558@users.noreply.github.com>
2023-11-06 14:49:16 -0700

    Started Getting UI Setup Base WPF

---
+++ FeBuddy/WPFUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFUI.ViewModels
+{
+    public class ShellViewModel: Screen
+    {
+    }
+}

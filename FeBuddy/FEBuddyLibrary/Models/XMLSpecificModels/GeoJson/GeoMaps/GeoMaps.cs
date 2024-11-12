87b0c7847c91b6a39b225d4361774888f0acee6c
Nikolas Boling <38259407+Nikolai558@users.noreply.github.com>
2024-11-12 16:07:59 -0700

    Added Comments

--- FeBuddy/FEBuddyLibrary/Models/XMLSpecificModels/GeoJson/GeoMaps/GeoMaps.cs
+++ FeBuddy/FEBuddyLibrary/Models/XMLSpecificModels/GeoJson/GeoMaps/GeoMaps.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace FEBuddyLibrary.Models.XMLSpecificModels.GeoJson.GeoMaps;
-
+/// <summary>
+/// Represents a collection of <see cref="GeoMap"/> instances that can be serialized or deserialized using XML.
+/// This class is marked as the root element in XML serialization and serves as a container for multiple geographic maps.
+/// </summary>
 [XmlRoot(ElementName = "GeoMaps")]
 public class GeoMaps
 {
+	/// <summary>
+	/// Gets or sets the list of <see cref="GeoMap"/> that belong to this collection.
+	/// </summary>
 	[XmlElement(ElementName = "GeoMap")]
 	public List<GeoMap> GeoMap { get; set; }
 }

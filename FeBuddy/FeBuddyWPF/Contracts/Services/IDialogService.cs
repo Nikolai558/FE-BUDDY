cac2ce7b17b2d806bf93ebb3e63fb75daa88ccb1
Nikolas Boling <38259407+Nikolai558@users.noreply.github.com>
2025-06-26 14:55:26 -0700

    Added Dialog Service
    This will make it easier to change how we want to show warning messages or informative messages to the user in the future.

---
+++ FeBuddy/FeBuddyWPF/Contracts/Services/IDialogService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeBuddyWPF.Contracts.Services
+{
+    public interface IDialogService
+    {
+        void ShowMessage(string message, string title = "Info");
+    }
+}

cd456e717a376a04fb9cd9876287f898610d61bf
Nikolas Boling <38259407+Nikolai558@users.noreply.github.com>
2025-06-26 14:36:51 -0700

    Some changes that were made a while back.

---
+++ FeBuddy/FEBuddyLibrary/Configurations/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using FEBuddyLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FEBuddyLibrary.Configurations;
+
+
+
+public class AppSettings : IappSettings
+{
+	public bool DevMode { get; set; }
+	public string OutputDirectory { get; set; }
+
+}

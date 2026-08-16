f57a581916173bdf56ee88a4b2556a3b4f074c7c
Nikolas Boling <38259407+Nikolai558@users.noreply.github.com>
2026-08-15 20:10:02 -0700

    Bug Fix - Log getting created next to EXE file
    Users can now install anywhere they want and the program will run properly because the log file isnt being created next to the EXE. It is instead being created in %localappdata%\FE-BUDDY\Logs

--- FeBuddyLibrary/Helpers/Logger.cs
+++ FeBuddyLibrary/Helpers/Logger.cs
@@ -7,7 +7,8 @@ namespace FeBuddyLibrary.Helpers
     {
         // TODO - Use actual Program Logger Framework.
 
-        public static readonly string _logFilePath = $"{Environment.CurrentDirectory}\\FE-BUDDY_LOG.txt";
+        private static readonly string _logDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "FE-BUDDY", "Logs");
+        public static readonly string _logFilePath = Path.Combine(_logDirectory, "FE-BUDDY_LOG.txt");
 
         public static void LogMessage(string level, string message)
         {
@@ -18,6 +19,8 @@ namespace FeBuddyLibrary.Helpers
 
         public static void CreateLogFile()
         {
+            Directory.CreateDirectory(_logDirectory);
+
             string logHeader = "This file may serve useful to the developers in the case of program issues. Please send this file with your bug report.";
 
             File.WriteAllText(_logFilePath, logHeader += "\n\n");
